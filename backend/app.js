7f1f8040a3f9bb5d61b0e65db9f98a314ca3212a
MangwonCassie <dudnfsla1@naver.com>
2024-04-11 19:13:47 +0900

    feat: 에러 핸들링 추가

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ import fileUpload from "express-fileupload";
 import jobRouter from "./routes/jobRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import applicationRouter from "./routes/applicationRoutes.js";
+import { dbConnection } from './database/dbConnection.js';
 
 const app = express();
 dotenv.config({path: "./config/config.env"});
@@ -35,5 +36,7 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/application", applicationRouter);
   
 app.use("/api/v1/job", jobRouter);
+
+dbConnection();
 
 export default app;
