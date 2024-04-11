26b1bdaa78735b174707a3ca38de515a59d8ac2a
MangwonCassie <dudnfsla1@naver.com>
2024-04-11 18:29:26 +0900

    feat: dbConnection 설정

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,9 @@ import express from 'express';
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
+import jobRouter from "./routes/jobRoutes.js";
+import userRouter from "./routes/userRoutes.js";
+import applicationRouter from "./routes/applicationRoutes.js";
 
 const app = express();
 dotenv.config({path: "./config/config.env"});
@@ -25,5 +28,12 @@ app.use(
       tempFileDir: "/tmp/",
     })
   );
+
+
+app.use("/api/v1/user", userRouter);
+  
+app.use("/api/v1/application", applicationRouter);
+  
+app.use("/api/v1/job", jobRouter);
 
 export default app;
