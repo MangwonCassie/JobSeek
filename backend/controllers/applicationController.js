0b7bd3e41644bd4ba8f26a8443367b095da6d310
MangwonCassie <dudnfsla1@naver.com>
2024-04-18 17:23:53 +0900

    feat: jobseekerGetAllApplications 메소드 추가

--- backend/controllers/applicationController.js
+++ backend/controllers/applicationController.js
@@ -22,6 +22,28 @@ export const employerGetAllApplications = catchAsyncError(async (req, res, next)
     })
 })
 
+
+export const jobseekerGetAllApplications = catchAsyncError(async (req, res, next) => {
+    const { role } = req.user;
+
+    if (role === "Employer") {
+        return next(
+            new ErrorHanlder(
+                "Employer is not allowed to access these resources!",
+                400
+            )
+        )
+    }
+
+    const { _id } = req.user;
+    const applications = await Application.find({ 'applicationId.user:': _id });
+    res.status(200).json({
+        success: true,
+        applications
+    })
+})
+
+
 export const jobseekerDeleteApplication = catchAsyncErrors(
     async (req, res, next) => {
         const { role } = req.user;
