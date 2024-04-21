45b4d6e91c954f83de1abb65d11118d506a29b4a
MangwonCassie <dudnfsla1@naver.com>
2024-04-21 16:23:05 +0900

    feat: app.jsx 라우팅 설정

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+export const context = createContext({
+  isAuthorized: false,
+})
+
+const AppWrapper = () => {
+  const [isAuthorized, setIsAuthorized] = useState(false);
+  const [user, setUser] = useState({}); // user정보 받아올 때 업데이트
+  return (
+    <context.Provider
+      value={{
+        isAuthorized,
+        setIsAuthorized,
+        user,
+        setUser
+      }}>
+      <App />
+    </context.Provider>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppWrapper />
   </React.StrictMode>,
 )
